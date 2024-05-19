cb6c98f30650f17884c2263250568dd879ef0be0
Renata Pulz <renata.pulz@raizen.com>
2024-05-19 04:37:35 -0300

    Adicionado funções pra buscar a data atual e gerar um pdf, adequação no layout de exibição das informações

--- src/calculo_salario_liquido.js
+++ src/calculo_salario_liquido.js
@@ -2,9 +2,10 @@ function calcularSalario(salarioBruto, valorINSS, valorIR) {
 
     const valeTransporte = salarioBruto * 0.06;
     const unimed = 25.00;
+    const outrosDescontos = valeTransporte + unimed;
     
-    const salario = salarioBruto - valorINSS - valorIR - valeTransporte - unimed;
-    return salario;
+    const salario = salarioBruto - outrosDescontos - valorINSS - valorIR;
+    return {outrosDescontos, salario};
 }
 
 module.exports = calcularSalario;
