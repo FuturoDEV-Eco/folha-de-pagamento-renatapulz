0e6600d5c4a418fd0ef22800ba8c885e9ae95b8e
Renata Pulz <renata.pulz@raizen.com>
2024-05-19 02:51:00 -0300

    Adicionado novos campos de digitação para o usuário e editado exibição das infos no terminal

--- src/index.js
+++ src/index.js
@@ -8,24 +8,34 @@ const input = readline.createInterface({
     output: process.stdout
 });
 
-input.question('Insira o salário bruto para calcular o valor a ser pago de INSS: ', (salarioBruto) => {
-    const valorINSS = calcularINSS(parseFloat(salarioBruto));
-    const valorIR = calcularIR(parseFloat(salarioBruto));
-    const salario = calcularSalario(parseFloat(salarioBruto), valorINSS, valorIR);
+input.question('Para iniciar, digite seu nome: ', (nome) => {
+    input.question('Digite seu CPF: ', (cpf) => {
+        input.question('Digite o mês de pagamento (de 1 a 12): ', (mesPgto) => {
+            input.question('Insira seu salário bruto: ', (salarioBruto) => {
+                const valorINSS = calcularINSS(parseFloat(salarioBruto));
+                const valorIR = calcularIR(parseFloat(salarioBruto));
+                const salario = calcularSalario(parseFloat(salarioBruto), valorINSS, valorIR);
 
-    if (valorINSS == 0) {
-        console.log("Atenção: O salário bruto excede o limite da tabela do INSS de R$ 7.786,02.\nConsulte site para ver o desconto nessa faixa salarial.");
-    } else {
-        console.log("Valor a ser pago de INSS:", valorINSS.toFixed(2));
-    }
-    
-    if (valorIR == 0) {
-        console.log("Isento de imposto de renda");
-    } else {
-        console.log("Valor a ser pago de Imposto de renda:", valorIR.toFixed(2));
-    }
+                console.log("--- Folha de Pagamento ---");
+                console.log("Nome: ", nome);
+                console.log("CPF: ", cpf);
+                console.log("Salário Bruto: R$", salarioBruto);
 
-    console.log("Salário a receber retirando os descontos de INSS, IR, Unimed e Vale transporte: ", salario.toFixed(2));
-    
-    input.close();
+                if (valorINSS == 0) {
+                    console.log("INSS: O salário bruto excede o limite da tabela do INSS de R$ 7.786,02.\nConsulte site para ver o desconto dessa faixa salarial.");
+                } else {
+                    console.log("INSS: R$", valorINSS.toFixed(2));
+                }
+
+                if (valorIR == 0) {
+                    console.log("Imposto de Renda: R$ 0");
+                } else {
+                    console.log("Imposto de Renda: R$", valorIR.toFixed(2));
+                }
+
+                console.log("Salário Líquido: R$", salario.toFixed(2));
+                input.close();
+            });
+        });
     });
+});
